51f11418d1a5daf626541314cb12fc7583064c97
MilesTin <1154520245@qq.com>
2019-12-03 23:16:42 +0800

    add commodity model and some serializers

--- backend/commodity_publish/account/models.py
+++ backend/commodity_publish/account/models.py
@@ -3,11 +3,15 @@ from rest_framework.serializers import ModelSerializer
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField(verbose_name="学号", max_length=13, primary_key=True)
+    stuId = models.CharField(verbose_name="学号", max_length=13, primary_key=True)
+    nickname = models.CharField(verbose_name="昵称", max_length=20)
     password = models.CharField(verbose_name="密码", max_length=100)
     phone = models.CharField(verbose_name="手机号", max_length=11, null=True)
-    college = models.CharField(verbose_name="学院", max_length=20, null=True)
+    campus = models.CharField(verbose_name="学院", max_length=20, null=True)
     class_num = models.CharField(verbose_name="班级号", max_length=20, null=True)
+
+    def __str__(self):
+        return self.nickname + " "  + self.stuId
 
     class Meta:
         verbose_name = "用户"
@@ -25,3 +29,7 @@ class UserSerializerWithPassword(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+class UserSerialzierWithCommodityPublished(ModelSerializer):
+
+    commodities =
--- backend/commodity_publish/order/models.py
+++ backend/commodity_publish/order/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from rest_framework.serializers import *
+from account.models import User
 # Create your models here.
+class Commodity(models.Model):
+    comId = models.BigAutoField(verbose_name="商品id", primary_key=True)
+    name = models.CharField(verbose_name="商品名", max_length=100)
+    publish_time = models.DateTimeField(verbose_name="发布时间", auto_created=True)
+    price = models.DecimalField(verbose_name="价格",max_digits=20, decimal_places=10)
+    description = models.TextField(verbose_name="描述", max_length=300)
+
+    stuId = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="商品发布者", null=True)
+
+    class Meta:
+        verbose_name = "商品"
+        verbose_name_plural = "商品"
+        ordering = ('comId', )
+
+
+    def __str__(self):
+        return "商品 " + self.name +" : " + str(self.comId)
+
+class CommoditySerializer(ModelSerializer):
+
+    class Meta:
+        model = Commodity
+        fields = "__all__"
