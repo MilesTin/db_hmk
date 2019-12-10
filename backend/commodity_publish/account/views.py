4af4ded04bf9774b7d7831c469951834f1004229
MilesTIn <115450245@qq.com>
2019-12-10 17:31:55 +0800

    fix the permission bug

--- backend/commodity_publish/account/views.py
+++ backend/commodity_publish/account/views.py
@@ -20,14 +20,17 @@ logger = logging.getLogger(__name__)
 
 class IsOwner(permissions.IsAuthenticated):
 
+
     def has_permission(self, request, view):
-        user = User.objects.get(pk=view.kwargs['pk'])
-
-        if request.user == user:
-            return True
-
-        else:
-            return super(self.__class__, self).has_permission(request, view)
+        if super(self.__class__, self).has_permission(request, view):
+            try:
+                user = User.objects.get(pk=view.kwargs['pk'])
+                if request.user == user:
+                    return True
+            except Exception as e:
+                #fixme:something bad may happen
+                pass
+        return False
 
     def has_object_permission(self, request, view, obj):
         if isinstance(obj, User):
@@ -37,28 +40,35 @@ class IsOwner(permissions.IsAuthenticated):
 
 #user视图集
 class UserViewSet(viewsets.ModelViewSet):
+    safe_fields = ("stuId", "nickname")
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    permission_classes = [permissions.IsAdminUser, IsOwner]
+    permission_classes_by_action = {
+        'create': [permissions.AllowAny],
+        'list': permission_classes,
+        'retrieve': [permissions.IsAuthenticatedOrReadOnly],
+        'update': permission_classes,
+        'destroy': permission_classes,
+    }
 
     #list, detail权限管理
     def get_permissions(self):
-        if self.action == "list":
-            return [permissions.IsAdminUser()]
-        elif self.action == "detail":
-            return [permissions.IsAdminUser(), IsOwner()]
-        else:
-            return super(self.__class__, self).get_permissions()
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
 
-    def detail(self, request, *args, **kwargs):
-        #FIXME:权限管理有点问题,2017能访问2019等等
-        user = self.get_object()
-        serializer = self.get_serializer(user, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    # @action(detail=True, methods=['post','in get'])
+    # def detail(self, request, *args, **kwargs):
+    #     #FIXME:权限管理有点问题,2017能访问2019等等
+    #     user = self.get_object()
+    #     serializer = self.get_serializer(user, data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     else:
+    #         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['post'], permission_classes=[permissions.AllowAny])
     def login(self, request, pk=None):
@@ -85,14 +95,32 @@ class UserViewSet(viewsets.ModelViewSet):
 
         return Response({"msg":"logout successful"}, status=status.HTTP_200_OK)
 
+    def create(self, request, *args, **kwargs):
 
-    @action(detail=False, methods=['post'], permission_classes=[permissions.AllowAny])
-    def register(self, request, *args, **kwargs):
         try:
-            response = self.create(request, *args, **kwargs)
+            response = super(self.__class__, self).create(request, *args, **kwargs)
             return response
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        user = self.get_object()
+        if IsOwner().has_permission(request,self):
+            return Response(self.get_serializer(user).data)
+        else:
+            serializer = self.get_serializer(user)
+            data = serializer.data
+            safe_data = {}
+
+            try:
+                for key in self.safe_fields:
+                    safe_data[key] = data[key]
+            except KeyError:
+                #fixme:这可能会出错
+                pass
+            return Response(safe_data)
+
+
 
 
 
